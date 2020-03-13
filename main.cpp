bb12bafc73f3d2d81e7bf5875403d8fc678b02cd
Alice (TrofimenkoAI) <alice@example.com>
2020-03-13 10:28:09 +0300

    summ & raz

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int a, b;
+    cout << "Enter A, B" << endl;
+    cin >> a >> b;
+    cout << "A + B = " << a + b << '\n' << "A - B = " << a - b << '\n';
     return 0;
 }
